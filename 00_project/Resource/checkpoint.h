652442210df1fef949a86cf0d4c875b6253b656f
TomoyaKanazaki <221083@st.yoshida-g.ac.jp>
2024-05-10 14:37:54 +0900

    チェックポイントを追加

---
+++ 00_project/Resource/checkpoint.h
@@ -0,0 +1,37 @@
+//==========================================
+//
+//  チェックポイント(checkpoint.h)
+//  Author : Tomoya Kanazaki
+//
+//==========================================
+#pragma once
+#include "objectModel.h"
+
+//==========================================
+//  クラス定義
+//==========================================
+class CCheckPoint : public CObjectModel
+{
+public:
+
+	// メンバ関数
+	CCheckPoint();
+	~CCheckPoint();
+
+	HRESULT Init(void) override; // 初期化
+	void Uninit(void) override; // 終了
+	void Update(const float fDeltaTime) override; // 更新
+	void Draw(CShader* pShader = nullptr) override; // 描画
+
+	// 静的メンバ関数
+	static CCheckPoint* Create(const D3DXVECTOR3& rPos, const D3DXVECTOR3& rRot); // 生成処理
+
+private:
+
+	// メンバ関数
+	void CollisionPlayer(void);	// プレイヤーとの当たり判定
+
+	// メンバ変数
+	bool m_bSave; // セーブフラグ
+
+};
