d3b553d43c406d2f0430e88aa6b022cc45109c86
RitsukiObara <221134@st.yoshida-g.ac.jp>
2024-05-14 09:47:46 +0900

    敵の状態の基盤クラスを作成

--- 00_project/Resource/enemy_nonestate.h
+++ 00_project/Resource/enemy_nonestate.h
@@ -1,1 +1,37 @@
-#pragma once
+//===================================
+//
+// 敵の状態(基盤)ヘッダー[enemy_nonestate.h]
+// Author 小原立暉
+//
+//===================================
+#ifndef _ENEMY_NONESTATE_H_
+#define _ENEMY_NONESTATE_H_
+//***********************************
+// インクルードファイル
+//***********************************
+#include "main.h"
+
+//-----------------------------------
+// 前方宣言
+//-----------------------------------
+class CEnemy;		// 敵
+
+// クラス定義(敵の状態の親クラス)
+class CEnemyStateNone
+{
+public:
+
+	explicit CEnemyStateNone(const CEnemy* pEnemy);	// コンストラクタ
+	virtual ~CEnemyStateNone();			// デストラクタ
+
+	virtual void Uninit(void);			// 終了処理
+
+	virtual void Process(void) = 0;		// 状態処理
+
+private:
+
+	// メンバ変数
+	const CEnemy* m_pEnemy;				// 本体の情報
+};
+
+#endif
