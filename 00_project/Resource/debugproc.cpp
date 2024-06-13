e5ed15d7479b153f874e2ad61b38b8a46896e89a
ShionSatone <221081@st.yoshida-g.ac.jp>
2024-06-13 10:55:54 +0900

    デバッグ表示の文字色変えたよー。目がチカチカするかも

--- 00_project/Resource/debugproc.cpp
+++ 00_project/Resource/debugproc.cpp
@@ -21,7 +21,7 @@ namespace
 	const int MAX_FLOATNUM = 4; // 小数点以下の表示桁数
 	const int MAX_DEBUGSTRING = 2048; // デバッグ表示の最大文字数
 	const char* FONT_PASS = "Terminal"; // 使用するフォント
-	const D3DXCOLOR FONT_COL = D3DCOLOR_RGBA(255, 255, 255, 255);	// フォント色
+	const D3DXCOLOR FONT_COL = D3DCOLOR_RGBA(250, 0, 180, 255);	// フォント色
 
 	// 静的変数
 	LPD3DXFONT m_pFont = nullptr; // デバッグフォントへのポインタ
