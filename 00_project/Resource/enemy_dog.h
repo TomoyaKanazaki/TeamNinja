e10a9f8bf4d167120120f7667eaf2f4e5d0ed3b1
RitsukiObara <221134@st.yoshida-g.ac.jp>
2024-06-18 10:11:45 +0900

    狼の基盤を作成 狼関係のテキストやモデルを追加

--- 00_project/Resource/enemy_dog.h
+++ 00_project/Resource/enemy_dog.h
@@ -1,1 +1,55 @@
-#pragma once
+//============================================================
+//
+//	犬敵ヘッダー [enemyDog.h]
+//	Author：小原立暉
+//
+//============================================================
+//************************************************************
+//	二重インクルード防止
+//************************************************************
+#ifndef _ENEMY_DOG_H_
+#define _ENEMY_DOG_H_
+
+//************************************************************
+//	インクルードファイル
+//************************************************************
+#include "enemy.h"
+
+//************************************************************
+//	クラス定義
+//************************************************************
+// 犬敵クラス
+class CEnemyDog : public CEnemy
+{
+public:
+
+	// モーション列挙
+	enum EMotion
+	{
+		MOTION_IDOL = 0,	// 待機モーション
+		MOTION_WALK,		// 歩行モーション
+		MOTION_FOUND,		// 発見モーション
+		MOTION_ATTACK,		// 攻撃モーション
+		MOTION_UPSET,		// 動揺モーション
+		MOTION_MAX			// この列挙型の総数
+	};
+
+	// コンストラクタ
+	CEnemyDog();
+
+	// デストラクタ
+	~CEnemyDog() override;
+
+	// オーバーライド関数
+	HRESULT Init(void) override;	// 初期化
+	void Uninit(void) override;		// 終了
+	void Update(const float fDeltaTime) override;	// 更新
+	void Draw(CShader* pShader = nullptr) override;	// 描画
+
+	void SetData(void) override;	// 情報の設定処理
+
+private:
+
+};
+
+#endif	// _ENEMY_CHASE_H_
