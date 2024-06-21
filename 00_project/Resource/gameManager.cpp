b7b4c15a805c57ac3cebea6b48337441b92b1f07
RitsukiObara <221134@st.yoshida-g.ac.jp>
2024-06-21 12:05:03 +0900

    門に当たり判定付けた

--- 00_project/Resource/gameManager.cpp
+++ 00_project/Resource/gameManager.cpp
@@ -114,7 +114,7 @@ HRESULT CGameManager::Init(void)
 // 小原用ギミック置き場
 #if 1
 	// 踏み台
-	CGimmick::Create(D3DXVECTOR3(2990.0f, 1.0f, 0.0f), D3DXVECTOR3(10.0f, 0.0f, 1600.0f), CGimmick::TYPE_STEP, 3);
+	CGimmick::Create(D3DXVECTOR3(2990.0f, 1.0f, 0.0f), D3DXVECTOR3(10.0f, 0.0f, 1600.0f), CGimmick::TYPE_STEP, 4);
 
 	// 重い扉
 	CGimmick::Create(D3DXVECTOR3(4500.0f, 301.0f, -300.0f), D3DXVECTOR3(400.0f, 0.0f, 100.0f), CGimmick::TYPE_HEAVYDOOR, 4);
