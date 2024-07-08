4a3d9bf42f3a1933acc6f386b573ef4a6840f5c3
TomoyaKanazaki <221083@st.yoshida-g.ac.jp>
2024-07-08 11:16:47 +0900

    植物管理クラスの実装

--- 00_project/Resource/multi_plant.cpp
+++ 00_project/Resource/multi_plant.cpp
@@ -5,6 +5,7 @@
 //
 //===========================================
 #include "multi_plant.h"
+#include "plant.h"
 
 //===========================================
 //  定数定義
@@ -16,6 +17,8 @@ namespace
 	{
 		"data\\TEXTURE\\test.png", // テストテクスチャ
 	};
+
+	const float PERMISSION = 0.02f; // 重なりの許容範囲
 }
 
 //===========================================
@@ -29,7 +32,9 @@ static_assert(NUM_ARRAY(TEXTURE_FILE) == CMultiPlant::TYPE_MAX, "ERROR : Type Co
 CMultiPlant::CMultiPlant() : CObject3D(),
 m_Type(CMultiPlant::TYPE_MAX),
 m_pos(VEC3_ZERO),
-m_size(VEC3_ZERO)
+m_size(VEC3_ZERO),
+m_nNum(0),
+m_bGrow(false)
 {
 }
 
@@ -66,6 +71,9 @@ void CMultiPlant::Uninit(void)
 //===========================================
 void CMultiPlant::Update(const float fDeltaTime)
 {
+	// 植物の生成
+	if (!m_bGrow) { Grow(); }
+
 	// 親クラスの更新
 	CObject3D::Update(fDeltaTime);
 }
@@ -82,7 +90,7 @@ void CMultiPlant::Draw(CShader* pShader)
 //===========================================
 //  生成処理
 //===========================================
-CMultiPlant* CMultiPlant::Create(const D3DXVECTOR3& rPos, const D3DXVECTOR3& rSize, const EType type)
+CMultiPlant* CMultiPlant::Create(const D3DXVECTOR3& rPos, const D3DXVECTOR3& rSize, const EType type, int nNum)
 {
 	// ギミックの生成
 	CMultiPlant* pPlant = new CMultiPlant;
@@ -102,6 +110,96 @@ CMultiPlant* CMultiPlant::Create(const D3DXVECTOR3& rPos, const D3DXVECTOR3& rSi
 	// タイプの設定
 	pPlant->m_Type = type;
 
+	// 生成数の設定
+	pPlant->m_nNum = nNum;
+
 	// 確保したアドレスを返す
 	return pPlant;
+}
+
+//===========================================
+//  花の生成
+//===========================================
+void CMultiPlant::Grow()
+{
+	// 生成フラグを立てる
+	m_bGrow = true;
+
+	// オブジェクトの生成座標保存用変数
+	D3DXVECTOR2* pos = new D3DXVECTOR2[m_nNum];
+	for (int i = 0; i < m_nNum; ++i) { pos[i] = VEC2_ZERO; }
+
+	// 必要な数生成する
+	for (int i = 0; i < m_nNum; ++i)
+	{
+		// x座標を設定
+		while (1)
+		{
+			// 乱数生成
+			float fTemp = ((float)rand() / (RAND_MAX * 0.5f)) - 1.0f;
+
+			// 重ならないようにする
+			bool bHit = false;
+			for (int j = 0; j < m_nNum; ++j)
+			{
+				// 重なりすぎた場合ループを抜ける
+				if (pos[j].x - fTemp < PERMISSION)
+				{
+					bHit = true;
+					break;
+				}
+			}
+
+			// 重なっていた場合もう一度
+			if (bHit) { continue; }
+
+			// 乱数を保存する
+			pos[i].x = fTemp;
+
+			// ループを抜ける
+			break;
+		}
+
+		// y座標を設定
+		while (1)
+		{
+			// 乱数生成
+			float fTemp = ((float)rand() / (RAND_MAX * 0.5f)) - 1.0f;
+
+			// 重ならないようにする
+			bool bHit = false;
+			for (int j = 0; j < m_nNum; ++j)
+			{
+				// 重なりすぎた場合ループを抜ける
+				if (pos[j].y - fTemp < PERMISSION)
+				{
+					bHit = true;
+					break;
+				}
+			}
+
+			// 重なっていた場合もう一度
+			if (bHit) { continue; }
+
+			// 乱数を保存する
+			pos[i].y = fTemp;
+
+			// ループを抜ける
+			break;
+		}
+
+		// 生成した乱数から座標を算出
+		D3DXVECTOR3 posPlant = D3DXVECTOR3
+		(
+			m_pos.x + (m_size.x * pos[i].x),
+			m_pos.y,
+			m_pos.z + (m_size.z * pos[i].y)
+		);
+
+		// 植物を生成
+		CPlant::Create(posPlant, TEXTURE_FILE[m_Type]);
+	}
+
+	// メモリの解放
+	delete[] pos;
 }
--- 00_project/Resource/multi_plant.h
+++ 00_project/Resource/multi_plant.h
@@ -31,12 +31,18 @@ public:
 	virtual void Draw(CShader* pShader = nullptr) override; // 描画
 
 	// 静的メンバ関数
-	static CMultiPlant* Create(const D3DXVECTOR3& rPos, const D3DXVECTOR3& rSize, const EType type); // 生成
+	static CMultiPlant* Create(const D3DXVECTOR3& rPos, const D3DXVECTOR3& rSize, const EType type, int nNum); // 生成
 
 private:
 
+	// メンバ関数
+	void Grow(); // 花の生成
+
+	// メンバ変数
 	EType m_Type; // 生成する植物の種類
 	D3DXVECTOR3 m_pos; // 中心座標
 	D3DXVECTOR3 m_size; // 生成範囲
+	int m_nNum; // 生成数
+	bool m_bGrow; // 植物生成フラグ
 
 };
