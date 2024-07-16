be83aa9f310720037983c81411bfa69f1aeb5e10
YuichFujita <221065@st.yoshida-g.ac.jp>
2024-07-16 17:02:41 +0900

    resultManager:うぇーーどの動きを昔のから頂戴してきた

--- 00_project/Resource/resultManager.h
+++ 00_project/Resource/resultManager.h
@@ -11,6 +11,11 @@
 #define _RESULTMANAGER_H_
 
 //************************************************************
+//	前方宣言
+//************************************************************
+class CObject2D;	// オブジェクト2Dクラス
+
+//************************************************************
 //	クラス定義
 //************************************************************
 // リザルトマネージャークラス
@@ -20,8 +25,16 @@ public:
 	// 状態列挙
 	enum EState
 	{
-		STATE_NONE = 0,	// 何もしない状態
-		STATE_MAX		// この列挙型の総数
+		STATE_NONE = 0,			// 何もしない状態
+		STATE_FADEIN,			// フェードイン状態
+		STATE_FADEIN_WAIT,		// フェードイン待機状態
+		STATE_FADEIN_ACCEL,		// フェードイン加速状態
+		STATE_WAIT,				// 待機状態
+		STATE_FADEOUT,			// フェードアウト状態
+		STATE_FADEOUT_WAIT,		// フェードアウト待機状態
+		STATE_FADEOUT_ACCEL,	// フェードアウト加速状態
+		STATE_END,				// 終了状態
+		STATE_MAX				// この列挙型の総数
 	};
 
 	// コンストラクタ
@@ -42,8 +55,20 @@ public:
 	static void Release(CResultManager *&prResultManager);	// 破棄
 
 private:
+	// メンバ関数
+	void UpdateFadeIn(const float fDeltaTime);			// フェードイン更新
+	void UpdateFadeInWait(const float fDeltaTime);		// フェードイン待機更新
+	void UpdateFadeInAccel(const float fDeltaTime);		// フェードイン加速更新
+	void UpdateWait(const float fDeltaTime);			// 待機更新
+	void UpdateFadeOut(const float fDeltaTime);			// フェードアウト更新
+	void UpdateFadeOutWait(const float fDeltaTime);		// フェードアウト待機更新
+	void UpdateFadeOutAccel(const float fDeltaTime);	// フェードアウト加速更新
+
 	// メンバ変数
-	EState m_state;	// 状態
+	CObject2D *m_pFade;	// フェード情報
+	EState m_state;		// 状態
+	float m_fMoveY;		// 縦移動量
+	float m_nCurTime;	// 現在の待機時間
 };
 
 #endif	// _RESULTMANAGER_H_
