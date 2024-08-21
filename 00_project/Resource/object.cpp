5f93830caa0c71b98c87463191e4b73a03183a07
丹野竜之介 <221047@st.yoshida-g.ac.jp>
2024-08-22 08:59:58 +0900

    シェーダーデバッグの痕跡

--- 00_project/Resource/object.cpp
+++ 00_project/Resource/object.cpp
@@ -757,7 +757,7 @@ void CObject::DrawAll(const EScene scene)
 	// デバッグ時のシェーダー切り替え	
 	if (pDeb->GetEnableShader())
 	{
-		DrawAll_ZShader(scene);
+	//	DrawAll_ZShader(scene);
 		DrawAll_ToonShadow(scene);
 		DrawAll_Compensate(scene);
 	}
