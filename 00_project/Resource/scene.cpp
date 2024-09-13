ac3d122ff374322e99f8679d88f7042e440400bc
RitsukiObara <221134@st.yoshida-g.ac.jp>
2024-09-13 12:20:33 +0900

    ロゴ画面何となく出した

--- 00_project/Resource/scene.cpp
+++ 00_project/Resource/scene.cpp
@@ -13,6 +13,7 @@
 #include "light.h"
 #include "camera.h"
 #include "effekseerManager.h"
+#include "sceneLogo.h"
 #include "sceneTitle.h"
 #include "sceneSelect.h"
 #include "sceneGame.h"
@@ -114,6 +115,10 @@ CScene *CScene::Create(EMode mode)
 	CScene *pScene = nullptr;	// シーン情報
 	switch (mode)
 	{ // モードごとの処理
+	case MODE_LOGO:
+		pScene = new CSceneLogo(mode);
+		break;
+
 	case MODE_TITLE:
 		pScene = new CSceneTitle(mode);
 		break;
